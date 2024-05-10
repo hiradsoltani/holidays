fdd85659dbd5dbe413ca80063b9014c434bcf237
hiradsoltani <hiradsoltani@gmail.com>
2024-05-10 15:30:39 -0400

    fix: update port number logic

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 import log from "loglevel";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 dotenv.config();
 
 app.use(morgan("combined"));
