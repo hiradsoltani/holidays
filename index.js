b6bfb27c91f970fa597781a53d8543596de0000b
hiradsoltani <hiradsoltani@gmail.com>
2024-05-10 15:25:30 -0400

    feat: first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import log from "loglevel";
+
+const app = express();
+const port = 3000;
+dotenv.config();
+
+app.use(morgan("combined"));
+app.use(bodyParser.urlencoded({ extended: true }));
+morgan;
+
+app.listen(port, (req, res) => {
+  console.log(`App started running on port ${port}.`);
+  console.log(process.env.API_KEY);
+});
